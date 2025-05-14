732a9d3818121737e268078b2ad951e1e9690db8
Marta <emeraldraven27@gmail.com>
2025-05-14 18:27:28 +0300

    feat: add responsive header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,21 +1,31 @@
+import Burger from '../Burger/burger';
 import Navigation from '../Navigation/navigation';
 import './Header.css';
-import { type ReactElement } from 'react';
+import { useState, type ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
 const items = ['Catalog', 'About', 'Login', 'Registration'];
 
 export default function Header(): ReactElement {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleHamburger = (): void => {
+    setIsOpen(!isOpen);
+    console.log('click');
+  };
+
   return (
     <>
       <header>
         <div className="container container-header">
+          <div className={isOpen ? 'hamburger open' : 'hamburger'} onClick={toggleHamburger}>
+            <Burger />
+          </div>
           <Link to="/" className="logo">
             {`${'space travel'}`.toUpperCase()}
           </Link>
 
           <div className="nav-container">
-            <Navigation items={items} className={'menu'} />
+            <Navigation className={isOpen ? 'menu open' : 'menu'} items={items} />
             <div className="icons-container">
               <div className="basket"></div>
             </div>

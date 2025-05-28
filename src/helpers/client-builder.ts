6181228bebaece069849e03b55f47b89f0c2de18
barmenski <1kiloom@gmail.com>
2025-05-29 00:44:40 +0300

    feat: add anon_token

--- src/helpers/client-builder.ts
+++ src/helpers/client-builder.ts
@@ -40,6 +40,7 @@ export const tokenCache = {
     return { token: '', expirationTime: 0 };
   },
   set: (token: TokenStore | AccessToken): void => {
+    console.log(token);
     if (isAccessToken(token)) {
       const expirationTime = Date.now() + token.expires_in * 1000;
       const adapted: TokenStore = {
@@ -49,7 +50,7 @@ export const tokenCache = {
       };
       localStorage.setItem('ctp_token', JSON.stringify(adapted));
     } else {
-      localStorage.setItem('ctp_token', JSON.stringify(token));
+      localStorage.setItem('ctp_anon_token', JSON.stringify(token));
     }
     globalThis.dispatchEvent(new Event('storage'));
   },
@@ -57,8 +58,8 @@ export const tokenCache = {
 
 function getOrCreateAnonymousId(): string {
   const stored = localStorage.getItem('ctp_anonymous_id');
-  const token = tokenCache.get();
-  if (!stored || !token.token) {
+  const valueAnonToken = localStorage.getItem('ctp_anon_token');
+  if (!stored || !valueAnonToken) {
     const newId = crypto.randomUUID();
     localStorage.setItem('ctp_anonymous_id', newId);
     return newId;
@@ -100,6 +101,17 @@ export const resetToAnonymous = (): void => {
     .build();
 
   currentApiRoot = createApiBuilderFromCtpClient(anonClient);
+  currentApiRoot
+    .withProjectKey({ projectKey: import.meta.env.VITE_CTP_PROJECT_KEY })
+    .products()
+    .get({ queryArgs: { limit: 1 } })
+    .execute()
+    .then(() => {
+      tokenCache.get();
+    })
+    .catch((error) => {
+      console.error('❌ Failed to initialize anonymous session:', error);
+    });
 };
 
 export const logout = (): void => {
@@ -127,3 +139,12 @@ let currentApiRoot: ApiRoot = createApiBuilderFromCtpClient(
     .withHttpMiddleware(httpMiddlewareOptions)
     .build(),
 );
+
+export const checkToken = (): void => {
+  const valueToken = localStorage.getItem('ctp_token');
+  const valueAnonToken = localStorage.getItem('ctp_anon_token');
+  if (!valueToken && !valueAnonToken) {
+    resetToAnonymous();
+  }
+};
+checkToken();

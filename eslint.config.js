72daaf561303d3eda6039b3933533ab5b292634e
Marta <emeraldraven27@gmail.com>
2025-06-01 23:14:08 +0300

    feat: add sliders and styles

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,10 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default tseslint.config(
+  ...pluginQuery.configs['flat/recommended'],
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
--- src/pages/ProductPage/product-page.tsx
+++ src/pages/ProductPage/product-page.tsx
@@ -1,10 +1,17 @@
 import type { JSX } from 'react';
 import { getProduct } from '../../api/get-product';
-import { useQuery } from '@tanstack/react-query';
 import './product-page.css';
+import { useQuery } from '@tanstack/react-query';
+import Slider from './swiper';
+
 // const product: Planet = await getProduct('flight-starship-2017-OF201');
 
 export default function ProductPage(): JSX.Element {
+  //flight-2017-OF201
+  // const queryClient = useQueryClient();
+  //flight-HD149026b
+  //flight-B6XX205
+  //flight-A890-V513
   const { isPending, isError, data, error } = useQuery({
     queryFn: () => getProduct('flight-2017-OF201'),
     queryKey: ['flight-2017-OF201'],
@@ -21,7 +28,7 @@ export default function ProductPage(): JSX.Element {
 
   const description = data.masterData.current.description['en-US'];
 
-  // const images = data.masterData.current.masterVariant.images;
+  const images = data.masterData.current.masterVariant.images;
 
   const price = Number(data.masterData.current.masterVariant.prices[0].value.centAmount) / 100;
 
@@ -33,7 +40,9 @@ export default function ProductPage(): JSX.Element {
     <>
       <div className="container-section-product">
         <div className="product-container">
-          <div className="slider-container"></div>
+          <div className="slider-container">
+            <Slider {...images}></Slider>
+          </div>
           <div className="product-info">
             <h1 className="product-name">{name}</h1>
             <div className="prices-container">

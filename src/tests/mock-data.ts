d4b5283f3d11034ef89442747caf1fbf40a6d3a0
Michael <fatlycka@gmail.com>
2025-06-10 22:11:51 +0300

    feat: add 30% test coverage

---
+++ src/tests/mock-data.ts
@@ -0,0 +1,46 @@
+export const mockData = {
+  facets: {},
+  limit: 20,
+  offset: 0,
+  count: 2,
+  total: 2,
+  results: [
+    {
+      attributes: [],
+      categories: [{ typeId: 'category', id: 'c511ccb3-552b-4222-95c8-6d56e8d2a992' }],
+      categoryOrderHints: {},
+      createdAt: '2025-05-04T12:20:49.262Z',
+      description: {
+        'en-US':
+          '<p>Orbiting within a radiant planetary ring, B6XX205 offers heart-shaped lakes, crystal caverns, and starry skies you’ll never forget.</p>',
+      },
+      hasStagedChanges: false,
+      id: '9a02a229-8e83-4fd4-8d5b-0d2bd03467fd',
+      key: 'flight-HD149026b',
+      lastModifiedAt: '2025-06-03T17:27:29.154Z',
+      masterVariant: {
+        attributes: [],
+        assets: [],
+        images: [
+          {
+            dimensions: { w: 560, h: 558 },
+            url: 'https://images.cdn.europe-west1.gcp.commercetools.com/fba92904-a8ac-4557-97e0-4066447536d3/simon-lee-XnGxTBij48-CRQysw5y.png',
+          },
+        ],
+        key: 'flight-HD149026b',
+        id: 1,
+        sku: 'flight-HD149026b',
+        prices: [{ id: 'd1531c5a-a855-4487-8057-761a1a93c358' }],
+      },
+      name: { 'en-US': 'Flight to the HD149026b' },
+      priceMode: 'Embedded',
+      productType: { typeId: 'product-type', id: 'b0d43ddc-1e3f-4293-8194-ad04866c9d3f' },
+      published: true,
+      searchKeywords: {},
+      slug: { 'en-US': 'flight-to-the-HD149026b' },
+      taxCategory: { typeId: 'tax-category', id: '052862c1-793a-49ff-a7ea-507f6d91a8a7' },
+      variants: [],
+      version: 52,
+    },
+  ],
+};
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    name: 'happy-dom',
-    environment: 'happy-dom',
+    name: 'jsdom',
+    environment: 'jsdom',
     setupFiles: ['src/tests/tests.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     exclude: ['**/*.config.js'],

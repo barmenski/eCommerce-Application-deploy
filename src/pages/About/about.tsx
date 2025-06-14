4872848a59bc718aa80e998d34bab9ba63c0b5e1
Marta <emeraldraven27@gmail.com>
2025-06-14 20:40:55 +0300

    feat: add basic layout f or about us page

--- src/pages/About/about.tsx
+++ src/pages/About/about.tsx
@@ -1,9 +1,60 @@
 import type { ReactElement } from 'react';
+import './about.css';
+import { Link } from 'react-router';
 
 export default function About(): ReactElement {
   return (
     <>
-      <h1 style={{ color: 'white' }}>Coming soon</h1>;
+      <main className="main">
+        <div className="container-section">
+          <h1>Our team</h1>
+          <div className="about-us-wrapper">
+            <div className="about-block">
+              <div className="one photo"></div>
+              <div className="name">x x</div>
+              <div className="title">front-end developer</div>
+              <div className="github-img"></div>
+              <div className="description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque maxime voluptatum
+                magnam fugit unde assumenda consectetur, aperiam, expedita vero amet reprehenderit
+                ducimus voluptates quos tempore recusandae! Molestiae inventore quidem est!
+              </div>
+            </div>
+            <div className="about-block">
+              <div className="two photo"></div>
+              <div className="name">x x</div>
+              <div className="title">front-end developer</div>
+              <div className="github-img"></div>
+              <div className="description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque maxime voluptatum
+                magnam fugit unde assumenda consectetur, aperiam, expedita vero amet reprehenderit
+                ducimus voluptates quos tempore recusandae! Molestiae inventore quidem est!
+              </div>
+            </div>
+            <div className="about-block">
+              <div className="three photo"></div>
+              <div className="name">x x</div>
+              <div className="title">front-end developer</div>
+              <div className="github-img"></div>
+              <div className="description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque maxime voluptatum
+                magnam fugit unde assumenda consectetur, aperiam, expedita vero amet reprehenderit
+                ducimus voluptates quos tempore recusandae! Molestiae inventore quidem est!
+              </div>
+            </div>
+          </div>
+          <h2>Collaboration</h2>
+          <div className="collaboration-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dolores repellendus
+            officia expedita dicta quis rerum! Mollitia provident aut magnam, voluptas sed vel quas,
+            molestiae, cumque voluptates ex quaerat quibusdam.
+          </div>
+
+          <Link to="https://rs.school/" target="_blank">
+            <div className="img-logo"></div>
+          </Link>
+        </div>
+      </main>
     </>
   );
 }

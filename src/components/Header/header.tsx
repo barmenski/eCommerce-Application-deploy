0fff44c7062c8b66eac8ac6e014832c9a911780d
Marta <emeraldraven27@gmail.com>
2025-06-15 19:40:43 +0300

    feat: add counter for the total number of items in a basket

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,11 +1,32 @@
+import { getActiveCart } from '../../api/get-active-cart';
 import Navigation from '../Navigation/navigation';
 import './Header.css';
-import { type ReactElement } from 'react';
+import { useEffect, useState, type ReactElement } from 'react';
 import { Link } from 'react-router';
 
 const items = ['Home', 'catalog', 'About', 'LogIn', 'SignUp', 'Profile', 'LogOut'];
 
 export default function Header(): ReactElement {
+  const [itemsCart, setItems] = useState(0);
+
+  useEffect(() => {
+    const onStorage = async (): Promise<void> => {
+      const activeCart = await getActiveCart();
+      const activeCartTotalNumber = activeCart.totalLineItemQuantity;
+
+      if (activeCartTotalNumber === undefined) {
+        setItems(0);
+      } else {
+        setItems(activeCartTotalNumber);
+      }
+    };
+    globalThis.addEventListener('storage', onStorage);
+
+    return (): void => {
+      globalThis.removeEventListener('storage', onStorage);
+    };
+  }, [itemsCart]);
+
   return (
     <>
       <header>
@@ -18,7 +39,7 @@ export default function Header(): ReactElement {
             <Navigation items={items} />
             <div className="icons-container">
               <Link to="/basket" className="basket"></Link>
-              <div className="items-count">0</div>
+              <div className="items-count">{itemsCart}</div>
             </div>
           </div>
         </div>
